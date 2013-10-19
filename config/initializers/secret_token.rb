2bbc81eda7382864a60174f3dfd9c11e30b4fab3
Kachina Gosselin <kachina@alum.mit.edu>
2013-10-19 13:12:18 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Githacker::Application.config.secret_key_base = '56e8473c5eb49c99751f4c96cfa31c8e336f808303db4fe5aeb4bf868c682ec69338084352fed1f985d46679cbeb5dd31231e625d671e013a1da044e278fd0a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Githacker::Application.config.session_store :cookie_store, key: '_githacker_session'
